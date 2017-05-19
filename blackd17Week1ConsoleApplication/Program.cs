fd90edf8ec51a6e2066a74041ffb0494887aad0c
David Black <black.david.t@gmail.com>
2017-05-18 23:07:02 -0400

    Adding the first week's project.

---
+++ blackd17Week1ConsoleApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackd17Week1ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Hello World!  My name is David.");
+
+            Console.ReadLine();
+        }
+    }
+}
