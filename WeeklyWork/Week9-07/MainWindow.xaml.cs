2f1d4b0c203c77574aca9336695694705fb8111e
David Black <black.david.t@gmail.com>
2017-07-22 12:33:00 -0400

    Framework for week 9 exercises

---
+++ WeeklyWork/Week9-07/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week9_07
+{
+    /// <summary>
+    /// Programming exercises for week 9 - CITP180 Summer 2017; this is CHAPTER 10
+    /// Exercise 7 is an expansion of exercise 6 to purchase bags 
+    /// Author: David Black
+    /// Date: 7/22/2017
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
